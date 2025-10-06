fc7070bcc1887c23180872e4be543a4f113d5177
ItsTower <120111320+SunveerIV@users.noreply.github.com>
2025-10-06 13:43:40 -0400

    Created network objects for multiplayer

---
+++ Assets/Scripts/Game/Behaviours/Managers/LevelMultiplayer.cs
@@ -0,0 +1,14 @@
+using Unity.Netcode;
+using Game.Interfaces;
+
+namespace Game.Behaviours.Managers {
+    public class LevelMultiplayer : NetworkBehaviour, ILevel {
+        public void EndGame() {
+            
+        }
+
+        public void UpdateScore() {
+            
+        }
+    }
+}
