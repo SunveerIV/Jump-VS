c807bd5f32bb623d43f1c7aff2e41bb305066434
ItsTower <120111320+SunveerIV@users.noreply.github.com>
2025-10-07 14:21:32 -0400

    Networked Platform now spawns

--- Assets/Scripts/Game/Behaviours/Managers/TwoPlayerLevel.cs
+++ Assets/Scripts/Game/Behaviours/Managers/TwoPlayerLevel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using Game.Interfaces;
@@ -10,12 +11,27 @@ namespace Game.Behaviours.Managers {
         [SerializeField] private PlatformMultiplayer platformMultiplayerPrefab;
         [SerializeField] private PlayerMultiplayer playerMultiplayerPrefab;
 
-        private void Awake() {
-            
-        }
-
+        private List<PlatformMultiplayer> platforms;
+        
+        private float highestPlatform;
+        
+        private int platformIndex = 0;
+        
         private void OnClientConnected(ulong clientId) {
+            NetworkManager manager = NetworkManager.Singleton;
+            if (!manager.IsServer) return;
+            if (manager.ConnectedClients.Count < 2) return;
             
+            platforms = new List<PlatformMultiplayer>();
+            highestPlatform = -1f;
+            InstantiatePlatform();
+        }
+        
+        private void InstantiatePlatform() {
+            highestPlatform += 2f;
+            PlatformMultiplayer platform = PlatformMultiplayer.Create(platformMultiplayerPrefab, new Vector2(UnityEngine.Random.Range(-2f, 2f), highestPlatform), platformIndex);
+            platformIndex++;
+            platforms.Add(platform);
         }
         
         private void OnEnable() {
--- Assets/Scripts/Game/Behaviours/Platforms/PlatformMultiplayer.cs
+++ Assets/Scripts/Game/Behaviours/Platforms/PlatformMultiplayer.cs
@@ -1,5 +1,14 @@
+using Unity.Netcode;
+using UnityEngine;
+
 namespace Game.Behaviours.Platforms {
     public class PlatformMultiplayer : PlatformBase {
-        
+
+        public static PlatformMultiplayer Create(PlatformMultiplayer prefab, Vector3 position, int index) {
+            PlatformMultiplayer platform = Instantiate(prefab, position, Quaternion.identity);
+            platform.Initialize(index);
+            platform.GetComponent<NetworkObject>().Spawn();
+            return platform;
+        }
     }
 }
