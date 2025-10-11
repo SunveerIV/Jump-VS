55445533b8f499c249418cebefb7375864406702
SunveerIV <120111320+SunveerIV@users.noreply.github.com>
2025-10-10 21:27:07 -0400

    Rising KillCollider Added (#21)
    Instead of hitting the bottom of the screen, now, the kill collider rises at a fixed rate.

--- Assets/Scripts/Game/Behaviours/Colliders/KillCollider.cs
+++ Assets/Scripts/Game/Behaviours/Colliders/KillCollider.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
+using Unity.Netcode;
 using Game.Utility;
+using Game.Constants;
 using Game.Interfaces;
 
 namespace Game.Behaviours.Colliders {
-    public class KillCollider : MonoBehaviour {
+    public class KillCollider : NetworkBehaviour {
         
-        public static KillCollider Create(KillCollider prefab) {
-            var collider = Instantiate(prefab, Camera.main.transform);
+        public static KillCollider Create(KillCollider prefab, bool multiplayer = false) {
+            var collider = Instantiate(prefab);
+            if (multiplayer) collider.NetworkObject.Spawn();
             return collider;
+        }
+
+        private void Update() {
+            Raise();
+        }
+
+        private void Raise() {
+            Vector3 position = transform.position;
+            position.y += Time.deltaTime * Level.RAISE_SPEED;
+            transform.position = position;
         }
 
         private void OnTriggerEnter2D(Collider2D other) {
