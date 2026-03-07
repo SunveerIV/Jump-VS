8003fed29588982a7f6c7fd122603d9287cdd550
SunveerIV <120111320+SunveerIV@users.noreply.github.com>
2026-03-06 21:13:56 -0500

    Add documentation

--- Assets/Scripts/Game/Utility/PhysicsTools.cs
+++ Assets/Scripts/Game/Utility/PhysicsTools.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 namespace Game.Utility {
     public static class PhysicsTools {
+        /// <summary>
+        /// Sticks the "this" object to the target object via FixedJoint2D
+        /// </summary>
+        /// <param name="self">The child object</param>
+        /// <param name="target">The parent object</param>
         public static void StickTo(this Component self, GameObject target) {
             if (target == null) return;
 
@@ -12,6 +17,10 @@ namespace Game.Utility {
             joint.connectedBody = target.GetComponent<Rigidbody2D>();
         }
 
+        /// <summary>
+        /// Unsticks the "this" object from all FixedJoint2D joints
+        /// </summary>
+        /// <param name="self">The child object</param>
         public static void Unstick(this Component self) {
             // Remove all FixedJoint2D components on this object
             foreach (var joint in self.GetComponents<FixedJoint2D>()) {
